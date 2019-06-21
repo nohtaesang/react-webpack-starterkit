7077d931bdce2970e9430d4df591792c40237590
nohtaesang <nohtaesang@naver.com>
2019-06-21 19:52:33 +0900

    react + webpack

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'js/app';
+import 'styles/index.css';
+import 'styles/index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<h3>Hello, React</h3>
+			<h4>Bye, React</h4>
+			<h5>change</h5>
+		</div>
+	);
+};
+
+export default App;
